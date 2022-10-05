c6822051afcee26f727bf4a2b807fe98dedcd616
DesiSuper <102736089+DesiSuper@users.noreply.github.com>
2022-10-05 12:00:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "TeamYourX")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001459999199'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamYourX')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
